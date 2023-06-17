bf74a7a1d231029c6266615cddbc8fef604080a6
zenkyuv <przemekgg2002@gmail.com>
2023-06-17 23:48:09 +0200

    add: create new board util

---
+++ s/elements/sidebar-panel/utils/create-new-board.ts
@@ -0,0 +1,22 @@
+import {Boards} from "../../../types"
+
+export function createNewBoard({setData, newBoardData}: {
+		setData: ({ boards }: Boards) => void
+		newBoardData: {
+			boardName: string
+			columns: string[]
+		}
+	}) {
+	setData({
+		boards: [{
+			name: newBoardData.boardName,
+			columns: [...newBoardData.columns.map(columnName => {
+				return {
+					name: columnName, tasks: [{
+						title: "", description: "", status: "", subtasks: [{ title: "", isCompleted: false }]
+					}]
+				}
+			})]
+		}]
+	})
+}

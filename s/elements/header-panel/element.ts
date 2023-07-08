29d30d08ccd17625ab8c237daa5b1a31a980180c
zenkyuv <przemekgg2002@gmail.com>
2023-07-08 23:40:06 +0200

    refactor: header-panel and use quick element

--- s/elements/header-panel/element.ts
+++ s/elements/header-panel/element.ts
@@ -1,10 +1,28 @@
-import {Data} from "../../types"
-import {LitElement, html} from "lit"
-import {mixinContextRequirement} from "@chasemoskal/magical"
+import {html} from "lit"
+import {QuickElement} from "@benev/frog"
 
-export class HeaderPanel extends mixinContextRequirement<Data>()(LitElement) {
+import {Context} from "../../main"
+import {styles} from "./style.css.js"
+import {HeaderMenu} from "./views/header-menu.js"
+import {AddNewTaskButton} from "./views/add-new-task-button.js"
+import {setup_context_manager} from "./utils/setup_context_manager.js"
+
+export const HeaderPanel = (context: Context) => class extends QuickElement {
+	static styles = styles
+	
 	render() {
-		const data = this.context
-		return html`<p>Halo</p>`
+
+		const context_manager = setup_context_manager(
+			context.data.value,
+			() => context.data.publish()
+		)
+
+		return html`
+			<header class=row>
+				<div class="main-header"><h1>Platform Launch</h1></div>
+				${AddNewTaskButton(context_manager)}
+				${HeaderMenu(context_manager)}
+			</header>
+		`
 	}
 }

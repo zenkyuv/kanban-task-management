b2728b8acc4a0d553d47097f7a87ff03f7c21dca
zenkyuv <przemekgg2002@gmail.com>
2023-07-16 23:55:56 +0200

    rework: use benev frog cues instead snapstate

--- s/main.ts
+++ s/main.ts
@@ -1,48 +1,45 @@
-import {snapstate} from "@chasemoskal/snapstate"
-import {css, registerElements, themeElements} from "@chasemoskal/magical"
+import {css} from "@chasemoskal/magical"
+import {CueGroup, pass_context_to_elements, register_elements, theme_elements, update_elements_on_cue_changes} from "@benev/frog"
 
 import data from "./data.js"
 import {MainPanel} from "./elements/main-panel/element.js"
 import {HeaderPanel} from "./elements/header-panel/element.js"
 import {SidebarPanel} from "./elements/sidebar-panel/element.js"
 
-const state = snapstate({
-	data,
-	openedBoard: "",
+const boardsData = {
+	boardsData: data,
+	activeBoardName: "Platform Launch",
 	themeMode: "light"
-})
-
-export const actions = {
-	setOpenedBoard(board: string) {
-		state.state.openedBoard = board
-	},
-	setThemeMode(mode: string) {
-		state.state.themeMode = mode
-	},
-	setData({boards}: typeof data) {
-		state.state.data.boards = [...state.state.data.boards, ...boards]
-	}
 }
 
-const context = {
-	state,
-	actions
+// trackChangesAndSaveToLocalStorage(state.track)
+
+export class Context {
+	cues = new CueGroup()
+	data = this.cues.create(boardsData)
 }
+
+export const context = new Context()
 
 const themeMode = css`
+
 	* {
 			margin: 0;
 			padding: 0;
 			box-sizing: border-box;
+			font-family: Plus Jakarta Sans;
 		}
 `
+const elements = {
+	MainPanel,
+	HeaderPanel,
+	SidebarPanel
+}
 
-registerElements(themeElements(
-	themeMode,
-	{
-		MainPanel: MainPanel.withContext(context),
-		HeaderPanel: HeaderPanel.withContext(context),
-		SidebarPanel: SidebarPanel.withContext(context)
-	},
-))
-
+register_elements(
+	theme_elements(themeMode,
+		update_elements_on_cue_changes(context.cues,
+			pass_context_to_elements(context, elements)
+		)
+	)
+)

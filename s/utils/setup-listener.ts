f365672388b0d56dd0be7a921288520d8b677422
zenkyuv <przemekgg2002@gmail.com>
2023-07-19 23:58:51 +0200

    add: setup listener helper function

---
+++ s/utils/setup-listener.ts
@@ -0,0 +1,14 @@
+export function setupListener(
+		target: EventTarget,
+		eventName: string,
+		listener: (e: any) => void,
+	) {
+
+	return () => {
+		target.addEventListener(eventName, listener)
+		return () => window.removeEventListener(
+			eventName,
+			listener,
+		)
+	}
+}

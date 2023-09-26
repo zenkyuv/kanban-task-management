41f652ecb137cba83a74a29d463a78d973c8f012
zenkyu <przemekgg2002@gmail.com>
2023-09-27 01:43:51 +0200

    remove: needless types

--- s/types.ts
+++ s/types.ts
@@ -1,13 +1,45 @@
-import { Snapstate } from "@chasemoskal/snapstate"
+export interface Task {
+	title: string
+	description: string
+	status: string
+	subtasks: Subtask[]
+}
 
-import data from "./data"
-import {actions} from "./main"
+export interface Subtask {
+	title: string
+	isCompleted: boolean
+}
 
-export interface Data {
-	state: Snapstate<{
-		data: typeof data
-		openedBoard: string
-		themeMode: string
-	}>
-	actions: typeof actions
+export interface Boards {
+	boards: Board[]
+}
+
+export interface Board {
+	name: string
+	columns: Column[]
+}
+// export type Track = typeof state.track
+
+export type Column = {
+	name: string
+	tasks: Task[]
+}
+
+export interface CreateBoardState {
+	remove_input: (i: number) => void
+	set_input_value: (i: number, e: InputEvent) => void
+	hide_create_board_panel: (e: PointerEvent) => void
+	add_input: () => void
+	get_inputs: () => string[]
+	get_board_data: (e: SubmitEvent) => {
+		boardName: string
+		boardColumnsNames: string[]
+	}
+}
+
+export type ActivePanel = "edit_board_panel" | "delete_board_panel" | ""
+
+export interface BoardData {
+	boardName: string
+	boardColumnsNames: string[]
 }
